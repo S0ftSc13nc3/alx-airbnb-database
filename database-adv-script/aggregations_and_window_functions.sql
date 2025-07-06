53847681e8791fb23360b399ad4ac4fe402e8bdc
Kopano Leshaba <138041617+S0ftSc13nc3@users.noreply.github.com>
2025-07-06 18:38:11 +0200

    aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -14,9 +14,16 @@ SELECT
 FROM bookings
 GROUP BY property_id;
 -- Rank properties based on the total number of bookings using RANK()
+WITH property_bookings AS (
+    SELECT
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+)
 SELECT
     property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM bookings
-GROUP BY property_id;
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM property_bookings;
+
