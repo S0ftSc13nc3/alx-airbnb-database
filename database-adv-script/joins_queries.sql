b3ac883652bab85afe3e4451873f49fd7184670a
Kopano Leshaba <138041617+S0ftSc13nc3@users.noreply.github.com>
2025-07-06 18:20:43 +0200

    Add files via upload

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -31,3 +31,25 @@ SELECT
     bookings.end_date
 FROM users
 FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+-- FULL OUTER JOIN workaround for MySQL (if needed)
+SELECT
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+LEFT JOIN users ON bookings.user_id = users.id;
